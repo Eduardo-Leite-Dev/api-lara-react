f3ce612eccb42a240d82ba9778dd994cef87a6bd
Eduardo <edusoares1992@gmail.com>
2022-03-30 21:31:30 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import akts from './assets/akts.svg'
 
 
 function App() {
   return (
-    <h1>Hello Word</h1>
+    <div className="container" > 
+
+      <div className="container-login" >
+
+        <div className="wrap-login">
+
+          <form action="" className="login-form" >
+
+            <span className="login-form-title" >Bem vindo! </span>
+            <span className="login-form-title" >
+              <img src= {akts} alt="login"/>
+
+              <div className='wrap-input' >
+                <input type="email"/>
+                <span className='' >
+
+                </span>
+
+              </div>
+
+            </span>
+
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
   );
 }
 
